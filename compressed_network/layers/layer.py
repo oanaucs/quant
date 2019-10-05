9a985145846843f4216926dbda11a03730b8cb8c
OanaPopescu <oanaucs@gmail.com>
2019-10-05 15:54:32 +0200

    fixed quantization

--- compressed_network/layers/layer.py
+++ compressed_network/layers/layer.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 
 import numpy as np
 
+from sklearn.cluster import KMeans 
+
 
 class layer_base(ABC):
     def __init__(self,
@@ -77,57 +79,56 @@ class layer_base(ABC):
 
     def quantize_weights(self, session, num_clusters):
         weight_values = session.run(self.weights)
+        zero_mask = np.ones(weight_values.shape)
+        zero_mask[weight_values == 0.0] = 0
 
         # assign to clusters
         self.centroids = np.linspace(
             start=np.min(weight_values), 
             stop=np.max(weight_values), 
-                num=num_clusters)
+                num=num_clusters - 1)
 
-        self.weight_clusters = np.digitize(
-            weight_values.flatten(), self.centroids)
-        self.weight_clusters -= 1
+        km = KMeans(n_clusters=num_clusters - 1, init=self.centroids.reshape(-1, 1), max_iter=10)
+        km.fit(weight_values.flatten().reshape(-1, 1))
 
-        quantized_weight_values = [self.centroids[i] for i in self.weight_clusters]
-        
-        self.weight_clusters = np.reshape(
-            self.weight_clusters, self.kernel_size)
+        print('labels', km.labels_)
+
+        self.labels = km.labels_ + 1
+        # set previous 0 weights to 0
+        self.labels = self.labels * zero_mask.flatten()
+        print('new labels', self.labels)
+        self.centroids = km.cluster_centers_
+        self.centroids = np.insert(self.centroids, 0, 0.0)
+        print('centroids', self.centroids)
+
+        quantized_weight_values = [self.centroids[int(i)] for i in self.labels]
         quantized_weight_values = np.reshape(
             quantized_weight_values, self.kernel_size)
-        
+
+        # print(quantized_weight_values)
+
+        self.labels = self.labels.reshape(self.kernel_size)
+
+        # print('layer', self.name, 'mse', np.square(weight_values, quantized_weight_values).mean())
+        # print(quantized_weight_values - weight_values)
+
         session.run(self.assign_clusters_op, feed_dict={
                     self.clusters_ph: quantized_weight_values})
 
-        self.recompute_centroids(weight_values)
-
-
-    def recompute_centroids(self, weight_values):
-        for i in range(0, len(self.centroids)):
-            weights_mask = np.zeros(self.weight_clusters.shape)
-            weights_mask[self.weight_clusters == i] = 1
-
-            count = np.count_nonzero(weights_mask)
-
-            if (count != 0):
-                weights_centroid_sum = np.sum(
-                    weight_values * weights_mask)
-                self.centroids[i] = weights_centroid_sum / count
 
     def quantize_gradients(self, gradient_values):
         gradients = np.zeros(gradient_values.shape)
 
-        for i in range(0, len(self.centroids)):
-            weights_mask = np.zeros(self.weight_clusters.shape)
-            weights_mask[self.weight_clusters == i] = 1            
+        for i in range(1, len(self.centroids)):
+            weights_mask = np.zeros(self.labels.shape)
+            weights_mask[self.labels == i] = 1           
             gradient_sum = np.sum(gradient_values * weights_mask)
             gradients += weights_mask * gradient_sum
+        # print(gradients)
         return gradients
 
     def assign_clusters(self, session):
-        session.run(self.assign_clusters_op, feed_dict={self.clusters_ph: self.weight_clusters})
+        session.run(self.assign_clusters_op, feed_dict={self.clusters_ph: self.labels})
         session.run(self.cast_op)
 
-        session.run(self.assign_bias_clusters_op, feed_dict={self.bias_clusters_ph: self.bias_clusters})
-        session.run(self.cast_bias_op)
-
-        return self.centroids
+        return self.labels
